dde3047fe2a77f89cdbc88d7667df3e1432dfd06
sorongosdev <dosora9@gmail.com>
2024-06-05 01:09:47 +0900

    답변등록에 장고 폼 적용

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 
 class QuestionForm(forms.ModelForm):
@@ -10,4 +10,12 @@ class QuestionForm(forms.ModelForm):
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
         }
--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 def index(request): # view 파일에 요청이 들어오면
     question_list = Question.objects.order_by('-create_date')
@@ -16,10 +17,19 @@ def detail(request, question_id):
 def answer_create(request,  question_id):
     """ pybo 답변등록"""
     question = get_object_or_404(Question, pk=question_id)
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save()
-    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail',  question_id=question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 def question_create(request):
     if request.method == 'POST':    # 요청이 post 방식이면
